42a75bb82e1bc09c6dd2474741caec2fd71c6085
andrewmock96 <moc21003@byui.edu>
2024-09-20 22:28:16 -0500

    m- Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradeString = Console.ReadLine();
+
+        int grade = int.Parse(gradeString);
+
+        string letter = "";
+        string sign = "";
+
+        if ( grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You earn a {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You'll do better next, but sadly you failed.");
+        }
     }
 }
