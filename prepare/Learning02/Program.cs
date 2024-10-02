c278896cbd759331d7b3c835f95a87b1f5d34cdc
andrewmock96 <moc21003@byui.edu>
2024-10-01 20:15:00 -0500

    d

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -19,8 +19,8 @@ class Program
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
-        myResume._jobs.add(job1);
-        myResume._jobs.add(job2);
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
 
         myResume.DisplayJobDetails();
     }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,7 +3,7 @@ using System;
 public class Resume
 {
     public string _name;
-    public List<Job>_jobs = new List<Job>();
+    public List<Job> _jobs = new List<Job>();
 
     public void DisplayJobDetails()
     {
@@ -11,7 +11,7 @@ public class Resume
         Console.WriteLine("Jobs:");
         foreach (Job job in _jobs)
         {
-            _jobs.DisplayJobDetails();
+            job.DisplayJobDetails();
         } 
     }
 }
