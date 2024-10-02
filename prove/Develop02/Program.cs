7958c4bf4c4391151f71e1cf25687cac5fd96a07
andrewmock96 <moc21003@byui.edu>
2024-10-01 22:12:56 -0500

    s

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string userResponseString = "";
+        int userResponse = 0;
+        Console.WriteLine("Welcome to the Journal Program! \nPlease Select one of the following choices (just enter the number)");
+        Console.Write("1. Write \n2. Display \n3. Load \n4. Save \n5. Quit. \nWhat would you like to do? ");
+        
+
+        while (userResponse != 5)
+        {
+        userResponseString = Console.ReadLine();
+        userResponse = int.Parse(userResponseString);
+            if (userResponse == 1)
+            {
+                Console.WriteLine("you chose 1");
+            }
+            if (userResponse == 2)
+            {
+                Console.WriteLine("you chose 2");
+            }
+            if (userResponse == 3)
+            {
+                Console.WriteLine("you chose 3");
+            }
+            if (userResponse == 4)
+            {
+                Console.WriteLine("you chose 4");
+            }
+            else
+            {
+                Console.WriteLine("Choose a vaild entry.");
+            }
+        }
+        Console.WriteLine("Thanks for using Develop02 Journal, GoodBye.");
+
     }
 }
