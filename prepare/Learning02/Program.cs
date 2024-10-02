187c85d379f79a4f470b086186ee6ed389a1be0d
andrewmock96 <moc21003@byui.edu>
2024-10-01 20:21:05 -0500

    D

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1.company = "Microsoft";
-        job1.jobTitle = "Software Engineer";
-        job1.startYear = 2019;
-        job1.endYear = 2022;
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._company = "Apple";
